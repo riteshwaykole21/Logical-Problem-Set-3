ac2ce0594c88259ae8b50dc1b446cdac867e6105
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-23 18:01:41 +0530

    [Ritesh]add :Return True

---
+++ BasicProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A Option Number");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    break;
+            }
+        }
+    }
+}
---
+++ BasicProblem/ReturnTrue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class ReturnTrue
+    {
+        public void Numbers()
+        {
+            Console.WriteLine("Enter First Number");
+            int First = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter A Second Number");
+            int Second = Convert.ToInt32(Console.ReadLine());   
+            if(First == 20)
+            {
+                Console.WriteLine(true);
+            }
+            else if(Second == 20)
+            {
+                Console.WriteLine(true);
+            }
+            else if(First + Second == 20)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false );
+            }
+        }
+    }
+}
